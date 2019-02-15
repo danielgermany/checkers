a8d1a28340272a2077b73d1024072b1698aee678
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-15 10:52:42 -0500

    The checker board is correctly drawn, the user can change the size of the board in the file by changing the value of 'boardSize'

--- DGcheckers.py
+++ DGcheckers.py
@@ -1,7 +1,7 @@
 from graphics import *
 from math import isclose
 
-boardSize = 10
+boardSize = 20
 
 def is_whole(n):
     if n % 1 == 0:
@@ -12,13 +12,19 @@ def is_whole(n):
 checkersWin = GraphWin("Checkers", 600,600)
 checkersWin.setCoords(0,0, 1000,1000)
 
-for x in range(boardSize):
- 
-    if is_whole(x/2) or x == 0:
-        checkerRect = Rectangle(Point(750 + (50 * x),750),Point(700 + (50 * x),700))
-        checkerRect.setFill(color_rgb(230,30,30))
-        checkerRect.draw(checkersWin)
-    else:
-        checkerRect = Rectangle(Point(750 + (50 * x),750),Point(700 + (50 * x),700))
-        checkerRect.setFill(color_rgb(0,0,0))
+for y in range(boardSize):
+    for x in range(boardSize):
+
+        checkerRect = Rectangle(Point(0 + (50 * x),950 - (50 * y)),Point(100 + (50 * x),1000 - (50 * y)))
+
+        if is_whole(y/2) or y == 0:
+            if is_whole(x/2) or x == 0:
+                checkerRect.setFill(color_rgb(230,30,30))
+            else:
+                checkerRect.setFill(color_rgb(0,0,0))
+        else:
+            if is_whole(x/2) or x == 0:
+                checkerRect.setFill(color_rgb(0,0,0))
+            else:
+                checkerRect.setFill(color_rgb(230,30,30))
         checkerRect.draw(checkersWin)
