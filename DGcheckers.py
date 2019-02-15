2d9de50b7636d6a7a158321ea7d19cc2505a0292
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-15 10:29:04 -0500

    A row of squares can be drawn

--- DGcheckers.py
+++ DGcheckers.py
@@ -1,4 +1,12 @@
 from graphics import *
 
+boardSize = 10
+
 checkersWin = GraphWin("Checkers", 600,600)
-shapesWin.setCoords(0,0, 1000,1000)
+checkersWin.setCoords(0,0, 1000,1000)
+
+for x in range(boardSize):
+    checkerRect = Rectangle(Point(750 + (50 * x),750),Point(700 + (50 * x),700))
+    checkerRect.setFill(color_rgb(230,30,30))
+    checkerRect.draw(checkersWin)
+    
