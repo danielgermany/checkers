77881eb57d62d0dd713568a75b1779785b430355
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-15 10:36:32 -0500

    The row now has alternating colors

--- DGcheckers.py
+++ DGcheckers.py
@@ -1,12 +1,24 @@
 from graphics import *
+from math import isclose
 
 boardSize = 10
+
+def is_whole(n):
+    if n % 1 == 0:
+        return True
+    else:
+        return False
 
 checkersWin = GraphWin("Checkers", 600,600)
 checkersWin.setCoords(0,0, 1000,1000)
 
 for x in range(boardSize):
-    checkerRect = Rectangle(Point(750 + (50 * x),750),Point(700 + (50 * x),700))
-    checkerRect.setFill(color_rgb(230,30,30))
-    checkerRect.draw(checkersWin)
-    
+ 
+    if is_whole(x/2) or x == 0:
+        checkerRect = Rectangle(Point(750 + (50 * x),750),Point(700 + (50 * x),700))
+        checkerRect.setFill(color_rgb(230,30,30))
+        checkerRect.draw(checkersWin)
+    else:
+        checkerRect = Rectangle(Point(750 + (50 * x),750),Point(700 + (50 * x),700))
+        checkerRect.setFill(color_rgb(0,0,0))
+        checkerRect.draw(checkersWin)
