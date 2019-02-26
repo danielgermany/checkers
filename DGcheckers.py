d39e2a792c338aca89238a40e1996142608d7823
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-26 12:06:29 -0500

    Board draws correctly

--- DGcheckers.py
+++ DGcheckers.py
@@ -1,30 +1,25 @@
 from graphics import *
-from math import isclose
 
-boardSize = 20
+boardSize = 8
+windowSize = 500
 
 def is_whole(n):
-    if n % 1 == 0:
-        return True
-    else:
-        return False
+    if n % 1 == 0: return True
+    else: return False
 
-checkersWin = GraphWin("Checkers", 600,600)
-checkersWin.setCoords(0,0, 1000,1000)
+checkersWin = GraphWin("Checkers", windowSize,windowSize)
+checkersWin.setCoords(0,0, windowSize,windowSize)
 
 for y in range(boardSize):
     for x in range(boardSize):
 
-        checkerRect = Rectangle(Point(0 + (50 * x),950 - (50 * y)),Point(100 + (50 * x),1000 - (50 * y)))
-
-        if is_whole(y/2) or y == 0:
-            if is_whole(x/2) or x == 0:
-                checkerRect.setFill(color_rgb(230,30,30))
-            else:
-                checkerRect.setFill(color_rgb(0,0,0))
+        checkerRect = Rectangle(Point((windowSize/10) * (x + 1),(windowSize/10) * (y + 1)),
+                                Point((windowSize/10) * (x + 2),(windowSize/10) * (y + 2)))
+        if is_whole(y/2):
+            if is_whole(x/2): checkerRect.setFill(color_rgb(230,30,30))
+            else: checkerRect.setFill(color_rgb(0,0,0))
         else:
-            if is_whole(x/2) or x == 0:
-                checkerRect.setFill(color_rgb(0,0,0))
-            else:
-                checkerRect.setFill(color_rgb(230,30,30))
+            if is_whole(x/2): checkerRect.setFill(color_rgb(0,0,0))
+            else: checkerRect.setFill(color_rgb(230,30,30))
+
         checkerRect.draw(checkersWin)
