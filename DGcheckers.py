aa2504c5407de5b18b0c3f0ee763324cf5b199a9
csPython <cspython@AD25TF04NGF1J-89.local>
2019-02-26 12:34:20 -0500

    Added a new way to draw grid

--- DGcheckers.py
+++ DGcheckers.py
@@ -1,5 +1,4 @@
 from graphics import *
-from DGcheckersFunc import is_whole
 
 print("Size of window?")
 windowSize = int(input())
@@ -7,16 +6,18 @@ windowSize = int(input())
 checkersWin = GraphWin("Checkers", windowSize,windowSize)
 checkersWin.setCoords(0,0, windowSize,windowSize)
 
+checkerBackground = Rectangle(Point(windowSize/10,windowSize/10),
+                              Point((windowSize/10)*9,(windowSize/10)*9))
+checkerBackground.setFill(color_rgb(0,0,0))
+checkerBackground.draw(checkersWin)
+
 for y in range(8):
     for x in range(8):
-
         checkerRect = Rectangle(Point((windowSize/10) * (x + 1),(windowSize/10) * (y + 1)),
                                 Point((windowSize/10) * (x + 2),(windowSize/10) * (y + 2)))
-        if is_whole(y/2):
-            if is_whole(x/2): checkerRect.setFill(color_rgb(230,30,30))
-            else: checkerRect.setFill(color_rgb(0,0,0))
+ 
+        if ((y/2) % 1 == 0):
+            if ((x/2) % 1 == 0): checkerRect.setFill(color_rgb(230,30,30))
         else:
-            if is_whole(x/2): checkerRect.setFill(color_rgb(0,0,0))
-            else: checkerRect.setFill(color_rgb(230,30,30))
-
+            if ((x/2) % 1 == 0) == False: checkerRect.setFill(color_rgb(230,30,30))
         checkerRect.draw(checkersWin)
